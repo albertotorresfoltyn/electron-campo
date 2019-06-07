de44bcfe70b638ec2c2d8c9ead39f7b148fa78ae
Alberto Torres <albertotorresfoltyn@gmail.com>
2019-06-06 22:32:25 -0300

    CARDS now work with onclick

--- app/components/CampoCard/index.js
+++ app/components/CampoCard/index.js
@@ -1,13 +1,15 @@
+/* eslint-disable react/prop-types */
+/* eslint-disable react/jsx-filename-extension */
 import React from 'react'
 import Card from 'react-bootstrap/Card'
 import Button from 'react-bootstrap/Button'
 import { ListGroup, ListGroupItem } from 'reactstrap';
 
-const Example = ({ campo }) => {
+const Example = ({campo, ...props }) => {
   const { IdCampo, Nombre, Descripcion, Superficie } = campo
 
   return (
-    <Card>
+    <Card {...props}>
       <Card.Header tag="h3" className="bg-success text-white">{Nombre}</Card.Header>
       <Card.Body className="text">
           <ListGroup>
@@ -17,7 +19,6 @@ const Example = ({ campo }) => {
             <ListGroupItem><strong>Descripción:</strong> {Descripcion}</ListGroupItem>
             <ListGroupItem><strong>Descripción:</strong> {Descripcion}</ListGroupItem>
           </ListGroup>
-       
       </Card.Body>
       <Card.Footer>
         <Button variant="primary">Entrar</Button>
--- app/routes.jsx
+++ app/routes.jsx
@@ -1,16 +1,18 @@
 /* eslint flowtype-errors/show-errors: 0 */
-import React from 'react';
-import { Switch } from 'react-router';
-import { Route, Redirect } from 'react-router-dom';
-import AppL from './containers/App';
-import HomePage from './containers/HomePage';
-import MenuedLayout from './containers/MenuedLayout';
+import React from 'react'
+import { Switch } from 'react-router'
+import { Route, Redirect } from 'react-router-dom'
+import AppL from './containers/App'
+import HomePage from './containers/HomePage'
+import PotrerosList from './containers/PotrerosList'
+import MenuedLayout from './containers/MenuedLayout'
 export default () => (
   <AppL>
     <Switch>
       <MenuedLayout>
         <Route exact path="/" component={HomePage} />
+        <Route exact path="/potreros/:campoid" component={PotrerosList} />
       </MenuedLayout>
     </Switch>
   </AppL>
-);
+)
