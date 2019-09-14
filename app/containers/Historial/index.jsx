236985fb573d734d277e1b510198103475854e23
Alberto Torres <albertotorresfoltyn@gmail.com>
2019-09-14 15:50:12 -0300

    new grid

--- app/containers/Historial/index.jsx
+++ app/containers/Historial/index.jsx
@@ -1,11 +1,8 @@
 // @flow
 import React, { Component } from 'react';
-
 import DataService from '../../services/DataService';
-
-import {
-  withRouter
-} from 'react-router-dom';
+import BootstrapTable from 'react-bootstrap-table-next';
+import { withRouter } from 'react-router-dom';
 
 
 class Historial extends Component {
@@ -13,34 +10,56 @@ class Historial extends Component {
     super(props);
 
 
-    //this.calcularTotalDetalle = this.calcularTotalDetalle.bind(this);
-    
+    // this.calcularTotalDetalle = this.calcularTotalDetalle.bind(this);
+
     this.state = {
       historialList: [],
     };
-
   }
 
   componentWillMount() {
-   //ALBERT
-    this.setState({ historialList: DataService.getAllDetalleByPotrero(2038)});
- 
+    // ALBERT
+    this.setState({ historialList: DataService.getAllDetalleByPotrero(2038) });
   }
 
-  
-  calcularTotalDetalle () {
+
+  calcularTotalDetalle() {
   }
 
   render() {
-    return (
-      <div>
-        
-      este es el historial
+    const columns = [{
+      dataField: 'id',
+      text: 'Product ID',
+      sort: true,
+    }, {
+      dataField: 'name',
+      text: 'Product Name',
+      sort: true,
+    }, {
+      dataField: 'price',
+      text: 'Product Price',
+      sort: true,
+    }];
 
-       
-      </div>
-    );
+    const defaultSorted = [{
+      dataField: 'name',
+      order: 'desc',
+    }];
+
+    const products = [
+      { id: 4, name: 'Item name', productPrice: 2104 },
+      { id: 5, name: 'Item name', productPrice: 2104 },
+      { id: 3, name: 'Item name', productPrice: 2103 },
+    ];
+
+    return (<BootstrapTable
+      bootstrap4
+      keyField="id"
+      data={products}
+      columns={columns}
+      defaultSorted={defaultSorted}
+    />);
   }
 }
 
-export default withRouter(Historial)
+export default withRouter(Historial);
