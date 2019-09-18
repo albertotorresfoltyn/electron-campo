20326ea9d665b020df149c418cedddf3b02cde72
Alberto Torres <albertotorresfoltyn@gmail.com>
2019-09-17 22:58:18 -0300

    spaces removed

--- app/containers/CambioCategoria/index.jsx
+++ app/containers/CambioCategoria/index.jsx
@@ -5,14 +5,10 @@ import BootstrapTable from 'react-bootstrap-table-next';
 import { withRouter } from 'react-router-dom';
 import paginationFactory from 'react-bootstrap-table2-paginator';
 
-
 class CambioCategoria extends Component {
   constructor(props) {
     super(props);
-
-
     // this.calcularTotalDetalle = this.calcularTotalDetalle.bind(this);
-
     this.state = {
      categoriasOrigen: [],
      categoriasDestino: [],
@@ -21,22 +17,12 @@ class CambioCategoria extends Component {
   }
 
   componentWillMount() {
-   
     const result  = DataService.getAllDetalleByPotrero(2038);
     this.setState({ historialList: result });
-    
   }
 
-
   render() {
-
-   
-   
-
-
-
     return (
-
       <button>LPM </button>
     );
   }
