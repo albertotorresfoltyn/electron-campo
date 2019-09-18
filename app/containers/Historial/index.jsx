5581040fc242422ab67b59d8460385f5b70d50c5
Alberto Torres <albertotorresfoltyn@gmail.com>
2019-09-17 22:37:32 -0300

    mooo motherfucker

--- app/containers/Historial/index.jsx
+++ app/containers/Historial/index.jsx
@@ -1,15 +1,13 @@
 // @flow
-import React, { Component } from 'react';
+import React, { Component, Fragment } from 'react';
 import DataService from '../../services/DataService';
 import BootstrapTable from 'react-bootstrap-table-next';
 import { withRouter } from 'react-router-dom';
 import paginationFactory from 'react-bootstrap-table2-paginator';
 
-
 class Historial extends Component {
   constructor(props) {
     super(props);
-
 
     // this.calcularTotalDetalle = this.calcularTotalDetalle.bind(this);
 
@@ -20,95 +18,96 @@ class Historial extends Component {
 
   componentWillMount() {
     // ALBERT
-    const result  = DataService.getAllDetalleByPotrero(2038);
+    const result = DataService.getAllDetalleByPotrero(2038);
     this.setState({ historialList: result });
-    console.log(result)
+    console.log(result);
   }
 
-
-  calcularTotalDetalle() {
-  }
+  calcularTotalDetalle() {}
 
   render() {
+    const priceFormatter = (cell) => {
+      console.log('cell -------------------------------');
 
-    function priceFormatter(cell, row, rowIndex) {
-      console.log("cell -------------------------------");
+      console.log(cell);
 
-          console.log(cell);
-         
-            cell.map((item) => {
-              return (
-              <span>
-              <strong style={ { color: 'red' } }>$ { item.type } </strong>
-              <strong style={ { color: 'red' } }>$ { item.amount } </strong>
-            </span>
-              )
-            }
-          
-          );
-      
-      
-    }
-     
+      const res = cell.map((item) => {
+        debugger;
+        return (
+          <div>
+            <strong style={{ color: 'green' }}>+ {`${item.amount} ${item.type}`} </strong>
+          </div>
+        );
+      });
+      return <Fragment>{res}</Fragment>;
+    };
 
     const columns = [
       {
         dataField: 'IdMovimiento',
         text: 'IdMovimiento',
         sort: true,
-      },{
-      dataField: 'Fecha',
-      text: 'Fecha',
-      sort: true,
-    }, {
-      dataField: 'TipoMovimiento',
-      text: 'Tipo Movimiento',
-      sort: true,
-    }, {
-      dataField: 'Motivo',
-      text: 'Motivo',
-      sort: true,
-    }, {
-      dataField: 'Observaciones',
-      text: 'Observaciones',
-      sort: false,
-    }, {
-      dataField: 'PotreroOrigen',
-      text: 'Potrero Origen',
-      sort: true,
-    },
-    , {
-      dataField: 'PotreroDestino',
-      text: 'Potrero Destino',
-      sort: true,
-    }
-    ,{
-      dataField: 'MovimientoDetalle',
-      text: 'Movimientos',
-      sort: false,
-      formatter:priceFormatter   
-    },{
-      dataField: 'PotreroDetalle1',
-      text: 'Detalle',
-      sort: false,
-    }];
+      },
+      {
+        dataField: 'Fecha',
+        text: 'Fecha',
+        sort: true,
+      },
+      {
+        dataField: 'TipoMovimiento',
+        text: 'Tipo Movimiento',
+        sort: true,
+      },
+      {
+        dataField: 'Motivo',
+        text: 'Motivo',
+        sort: true,
+      },
+      {
+        dataField: 'Observaciones',
+        text: 'Observaciones',
+        sort: false,
+      },
+      {
+        dataField: 'PotreroOrigen',
+        text: 'Potrero Origen',
+        sort: true,
+      },,
+      {
+        dataField: 'PotreroDestino',
+        text: 'Potrero Destino',
+        sort: true,
+      },
+      {
+        dataField: 'MovimientoDetalle',
+        text: 'Movimientos',
+        sort: false,
+        formatter: priceFormatter,
+      },
+      {
+        dataField: 'PotreroDetalle1',
+        text: 'Detalle',
+        sort: false,
+      },
+    ];
 
-    const defaultSorted = [{
-      dataField: 'Fecha',
-      order: 'desc',
-    }];
+    const defaultSorted = [
+      {
+        dataField: 'Fecha',
+        order: 'desc',
+      },
+    ];
 
-
-
-    return (<BootstrapTable
-   
-      bootstrap4
-      keyField="IdMovimiento"
-      data={this.state.historialList}
-      columns={columns}
-      defaultSorted={defaultSorted}
-      pagination={ paginationFactory() }
-    />);
+    return (
+      <BootstrapTable
+        bootstrap4
+        keyField="IdMovimiento"
+        data={this.state.historialList}
+        columns={columns}
+        defaultSorted={defaultSorted}
+        pagination={paginationFactory()}
+      />
+    );
   }
 }
 
