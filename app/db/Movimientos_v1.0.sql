f3e9eafefdc4225cb0688b0452cf8c278d074993
eugeniagalarza <eugeniagalarza@outlook.com>
2019-11-09 15:24:51 -0300

    carga de datos iniciales en Base de datos

---
+++ app/db/Movimientos_v1.0.sql
@@ -0,0 +1,55 @@
+delete from Movimiento;    
+delete from sqlite_sequence where name='Movimiento';
+
+INSERT INTO Movimiento (IdPotrero, Fecha, Motivo, Observaciones,MovimientoDetalle, PotreroDetalle, PotreroOrigen, PotreroDestino, TipoMovimiento)
+VALUES( 1, '11/11/2019','','Carga Inicial','[{"type":"VACA","amount":165},{"type":"TERNERO","amount":624}]','[{"type":"VACA","amount":165},{"type":"TERNERO","amount":624}]','',1,'INGRESO' );
+
+INSERT INTO Movimiento (IdPotrero, Fecha, Motivo, Observaciones,MovimientoDetalle, PotreroDetalle, PotreroOrigen, PotreroDestino, TipoMovimiento)
+VALUES( 2, '11/11/2019','','Carga Inicial','[{"type":"VAQUILLITA","amount":217}]','[{"type":"VAQUILLITA","amount":217}]','',2,'INGRESO' );
+
+INSERT INTO Movimiento (IdPotrero, Fecha, Motivo, Observaciones,MovimientoDetalle, PotreroDetalle, PotreroOrigen, PotreroDestino, TipoMovimiento)
+VALUES( 3, '11/11/2019','','Carga Inicial','[{"type":"VAQUILLITA","amount":156}]','[{"type":"VAQUILLITA","amount":156}]','',3,'INGRESO' );
+
+INSERT INTO Movimiento (IdPotrero, Fecha, Motivo, Observaciones,MovimientoDetalle, PotreroDetalle, PotreroOrigen, PotreroDestino, TipoMovimiento)
+VALUES( 4, '11/11/2019','','Carga Inicial','[{"type":"VAQUILLITA","amount":184}]','[{"type":"VAQUILLITA","amount":184}]','',4,'INGRESO' );
+
+INSERT INTO Movimiento (IdPotrero, Fecha, Motivo, Observaciones,MovimientoDetalle, PotreroDetalle, PotreroOrigen, PotreroDestino, TipoMovimiento)
+VALUES( 5, '11/11/2019','','Carga Inicial','[{"type":"VACA","amount":293}]','[{"type":"VACA","amount":293}]','',5,'INGRESO' );
+
+INSERT INTO Movimiento (IdPotrero, Fecha, Motivo, Observaciones,MovimientoDetalle, PotreroDetalle, PotreroOrigen, PotreroDestino, TipoMovimiento)
+VALUES( 6, '11/11/2019','','Carga Inicial','[{"type":"VACA","amount":195}]','[{"type":"VACA","amount":195}]','',6,'INGRESO' );
+
+INSERT INTO Movimiento (IdPotrero, Fecha, Motivo, Observaciones,MovimientoDetalle, PotreroDetalle, PotreroOrigen, PotreroDestino, TipoMovimiento)
+VALUES( 7, '11/11/2019','','Carga Inicial','[{"type":"VACA","amount":260}]','[{"type":"VACA","amount":260}]','',7,'INGRESO' );
+
+INSERT INTO Movimiento (IdPotrero, Fecha, Motivo, Observaciones,MovimientoDetalle, PotreroDetalle, PotreroOrigen, PotreroDestino, TipoMovimiento)
+VALUES( 8, '11/11/2019','','Carga Inicial','[{"type":"TORO","amount":134},{"type":"VACA","amount":327},{"type":"TERNERA","amount":327}]','[{"type":"TORO","amount":134},{"type":"VACA","amount":327},{"type":"TERNERA","amount":327}]','',8,'INGRESO' );
+
+INSERT INTO Movimiento (IdPotrero, Fecha, Motivo, Observaciones,MovimientoDetalle, PotreroDetalle, PotreroOrigen, PotreroDestino, TipoMovimiento)
+VALUES( 9, '11/11/2019','','Carga Inicial','[{"type":"VAQUILLONA","amount":180},{"type":"VAQUILLITA","amount":118}]','[{"type":"VAQUILLONA","amount":180},{"type":"VAQUILLITA","amount":118}]','',9,'INGRESO' );
+
+INSERT INTO Movimiento (IdPotrero, Fecha, Motivo, Observaciones,MovimientoDetalle, PotreroDetalle, PotreroOrigen, PotreroDestino, TipoMovimiento)
+VALUES( 10, '11/11/2019','','Carga Inicial','[{"type":"VAQUILLONA","amount":310},{"type":"VAQUILLITA","amount":117}]','[{"type":"VAQUILLONA","amount":310},{"type":"VAQUILLITA","amount":117}]','',10,'INGRESO' );
+
+INSERT INTO Movimiento (IdPotrero, Fecha, Motivo, Observaciones,MovimientoDetalle, PotreroDetalle, PotreroOrigen, PotreroDestino, TipoMovimiento)
+VALUES( 11, '11/11/2019','','Carga Inicial','[{"type":"NOVILLITO","amount":231},{"type":"VACA","amount":160}]','[{"type":"NOVILLITO","amount":231},{"type":"VACA","amount":160}]','',11,'INGRESO' );
+
+INSERT INTO Movimiento (IdPotrero, Fecha, Motivo, Observaciones,MovimientoDetalle, PotreroDetalle, PotreroOrigen, PotreroDestino, TipoMovimiento)
+VALUES( 12, '11/11/2019','','Carga Inicial','[{"type":"VACA","amount":254}]','[{"type":"VACA","amount":254}]','',11,'INGRESO' );
+
+INSERT INTO Movimiento (IdPotrero, Fecha, Motivo, Observaciones,MovimientoDetalle, PotreroDetalle, PotreroOrigen, PotreroDestino, TipoMovimiento)
+VALUES( 13, '11/11/2019','','Carga Inicial','[]','[]','',13,'INGRESO' );
+
+INSERT INTO Movimiento (IdPotrero, Fecha, Motivo, Observaciones,MovimientoDetalle, PotreroDetalle, PotreroOrigen, PotreroDestino, TipoMovimiento)
+VALUES( 14, '11/11/2019','','Carga Inicial','[{"type":"VACA","amount":312}]','[{"type":"VACA","amount":312}]','',14,'INGRESO' );
+
+INSERT INTO Movimiento (IdPotrero, Fecha, Motivo, Observaciones,MovimientoDetalle, PotreroDetalle, PotreroOrigen, PotreroDestino, TipoMovimiento)
+VALUES( 15, '11/11/2019','','Carga Inicial','[]','[]','',15,'INGRESO' );
+
+INSERT INTO Movimiento (IdPotrero, Fecha, Motivo, Observaciones,MovimientoDetalle, PotreroDetalle, PotreroOrigen, PotreroDestino, TipoMovimiento)
+VALUES( 16, '11/11/2019','','Carga Inicial','[{"type":"VACA","amount":155}]','[{"type":"VACA","amount":155}]','',16,'INGRESO' );
+
+INSERT INTO Movimiento (IdPotrero, Fecha, Motivo, Observaciones,MovimientoDetalle, PotreroDetalle, PotreroOrigen, PotreroDestino, TipoMovimiento)
+VALUES( 17, '11/11/2019','','Carga Inicial','[{"type":"VAQUILLITA","amount":315},{"type":"TERNERA","amount":315}]','[{"type":"VAQUILLITA","amount":315},{"type":"TERNERA","amount":315}]','',17,'INGRESO' );
+
+
---
+++ app/db/Potreros_v1.0.sql
@@ -0,0 +1,53 @@
+delete from Potrero;    
+delete from sqlite_sequence where name='Potrero';
+
+INSERT INTO Potrero (IdCampo, Nombre, Descripcion, Superficie,Calidad, Codigo, Categoria, Rendimiento, CantidadSaleros, CantidadAguadas, CargaSoportada, Orden)
+VALUES( 1, 'Potrero 1' , 'Descripcion Pendiente', 629.0, 'Bueno', '1', 'Cat1', 'Bueno' ,4,	5,	10,	1);
+
+INSERT INTO Potrero (IdCampo, Nombre, Descripcion, Superficie,Calidad, Codigo, Categoria, Rendimiento, CantidadSaleros, CantidadAguadas, CargaSoportada, Orden)
+VALUES( 1, 'Potrero 2a' , 'Descripcion Pendiente', 190.0, 'Mala', '2a', 'Cat2', 'Malo',1,10,20,2);
+
+INSERT INTO Potrero (IdCampo, Nombre, Descripcion, Superficie,Calidad, Codigo, Categoria, Rendimiento, CantidadSaleros, CantidadAguadas, CargaSoportada, Orden)
+VALUES( 1, 'Potrero 2b' , 'Descripcion Pendiente', 300.0, 'Regular', '2b', 'Cat3', 'Regular' ,2,	15,	30,	3);
+
+INSERT INTO Potrero (IdCampo, Nombre, Descripcion, Superficie,Calidad, Codigo, Categoria, Rendimiento, CantidadSaleros, CantidadAguadas, CargaSoportada, Orden)
+VALUES( 1, 'Potrero 2c' , 'Descripcion Pendiente', 200.0, 'Bueno', '2c', 'Cat4', 'Malo' ,1,20,	40,	4);
+
+INSERT INTO Potrero (IdCampo, Nombre, Descripcion, Superficie,Calidad, Codigo, Categoria, Rendimiento, CantidadSaleros, CantidadAguadas, CargaSoportada, Orden)
+VALUES( 1, 'Potrero 3' , 'Descripcion Pendiente', 310.0, 'Bueno', '3', 'Cat5', 'Regular' ,3,30,	35,	5);
+
+INSERT INTO Potrero (IdCampo, Nombre, Descripcion, Superficie,Calidad, Codigo, Categoria, Rendimiento, CantidadSaleros, CantidadAguadas, CargaSoportada, Orden)
+VALUES( 1, 'Potrero 4' , 'Descripcion Pendiente', 846.0, 'Bueno', '4', 'Cat6', 'Bueno' ,4,40,	60,	6);
+
+INSERT INTO Potrero (IdCampo, Nombre, Descripcion, Superficie,Calidad, Codigo, Categoria, Rendimiento, CantidadSaleros, CantidadAguadas, CargaSoportada, Orden)
+VALUES( 1, 'Potrero 5' , 'Descripcion Pendiente', 593.0, 'Bueno', '5', 'Cat7', 'Regular' ,4,55,	80,	7);
+
+INSERT INTO Potrero (IdCampo, Nombre, Descripcion, Superficie,Calidad, Codigo, Categoria, Rendimiento, CantidadSaleros, CantidadAguadas, CargaSoportada, Orden)
+VALUES( 1, 'Potrero 6' , 'Descripcion Pendiente', 486.0, 'Bueno', '6', 'Cat8', 'Malo' ,3,60,90,8);
+
+INSERT INTO Potrero (IdCampo, Nombre, Descripcion, Superficie,Calidad, Codigo, Categoria, Rendimiento, CantidadSaleros, CantidadAguadas, CargaSoportada, Orden)
+VALUES( 1, 'Potrero 7' , 'Descripcion Pendiente', 725.0, 'Bajo', '7', 'Cat9', 'Bueno' ,2,85,140,9);
+
+INSERT INTO Potrero (IdCampo, Nombre, Descripcion, Superficie,Calidad, Codigo, Categoria, Rendimiento, CantidadSaleros, CantidadAguadas, CargaSoportada, Orden)
+VALUES( 1, 'Potrero 8' , 'Descripcion Pendiente', 659.0, 'Regular', '8', 'Cat10', 'Regular' ,2,80,130,10);
+
+INSERT INTO Potrero (IdCampo, Nombre, Descripcion, Superficie,Calidad, Codigo, Categoria, Rendimiento, CantidadSaleros, CantidadAguadas, CargaSoportada, Orden)
+VALUES( 1, 'Potrero 9' , 'Descripcion Pendiente', 467.0, 'Bueno', '9', 'Cat11', 'Regular' ,2,65,100,11);
+
+INSERT INTO Potrero (IdCampo, Nombre, Descripcion, Superficie,Calidad, Codigo, Categoria, Rendimiento, CantidadSaleros, CantidadAguadas, CargaSoportada, Orden)
+VALUES( 1, 'Potrero 10' , 'Descripcion Pendiente', 239.0, 'Bueno', '10', 'Cat12', 'Malo' ,2,45,70,12);
+
+INSERT INTO Potrero (IdCampo, Nombre, Descripcion, Superficie,Calidad, Codigo, Categoria, Rendimiento, CantidadSaleros, CantidadAguadas, CargaSoportada, Orden)
+VALUES( 1, 'Potrero 13' , 'Descripcion Pendiente', 381.0, 'Bueno', '13', 'Cat13', 'Bueno' ,0,70,110,13);
+
+INSERT INTO Potrero (IdCampo, Nombre, Descripcion, Superficie,Calidad, Codigo, Categoria, Rendimiento, CantidadSaleros, CantidadAguadas, CargaSoportada, Orden)
+VALUES( 1, 'Potrero 14' , 'Descripcion Pendiente', 496.0, 'Regular', '14', 'Cat14', 'Malo' ,120,75,120,14);
+
+INSERT INTO Potrero (IdCampo, Nombre, Descripcion, Superficie,Calidad, Codigo, Categoria, Rendimiento, CantidadSaleros, CantidadAguadas, CargaSoportada, Orden)
+VALUES( 1, 'Potrero 17' , 'Descripcion Pendiente', 63.0, 'Bueno', '17', 'Cat15', 'Bueno' ,2,5,5,15);
+
+INSERT INTO Potrero (IdCampo, Nombre, Descripcion, Superficie,Calidad, Codigo, Categoria, Rendimiento, CantidadSaleros, CantidadAguadas, CargaSoportada, Orden)
+VALUES( 1, 'Potrero 18' , 'Descripcion Pendiente',44.0, 'Bueno', '18', 'Cat16', 'Bueno' ,3,5,5,16);
+
+INSERT INTO Potrero (IdCampo, Nombre, Descripcion, Superficie,Calidad, Codigo, Categoria, Rendimiento, CantidadSaleros, CantidadAguadas, CargaSoportada, Orden)
+VALUES( 1, 'Potrero 20' , 'Descripcion Pendiente',262.0, 'Bueno', '20', 'Cat17', 'Bueno' ,3,5,5,17);
